85a478a27667d3d4e3b29e8677102ef9f2da8034
Dev_POC <lfalandry@hotmail.com>
2025-06-08 20:06:13 +0200

    Changement pour publier sur git page

--- src/app/en-cours-de-construction/en-cours-de-construction.ts
+++ src/app/en-cours-de-construction/en-cours-de-construction.ts
@@ -4,8 +4,9 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-en-cours-de-construction',
   imports: [RouterModule],
+  standalone: true,
   templateUrl: './en-cours-de-construction.html',
-  styleUrl: './en-cours-de-construction.css'
+  styleUrls: ['./en-cours-de-construction.css']
 })
 export class EnCoursDeConstruction {
 

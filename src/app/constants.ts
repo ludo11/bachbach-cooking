6ef7e166440e4a331a1978e24882e848b9a14b48
Dev_POC <lfalandry@hotmail.com>
2025-06-08 18:35:48 +0200

    menu

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
 export const APP_CONSTANTS = {
   APP_NAME: 'Bach Cooking',
   API_URL: 'https://api.monsite.com',
-  FOOTER_TEXT: '© 2025 - La cuisine des débordés'
+  FOOTER_TEXT: '#LCDD © 2025 - La cuisine des débordés'
 };
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { APP_CONSTANTS } from '../constants';
 import { RouterModule } from '@angular/router';
 
@@ -14,8 +14,23 @@ appName = APP_CONSTANTS.APP_NAME;
 menuOpen = false;
 
 
-
+constructor(private eRef: ElementRef) {}
 toggleMenu() {
   this.menuOpen = !this.menuOpen;
 }
+
+closeMenu() {
+  // Ne ferme que si on est sur mobile
+  if (window.innerWidth <= 768) {
+    this.menuOpen = false;
+  }
+}
+
+ @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent) {
+    const clickedInside = this.eRef.nativeElement.contains(event.target);
+    if (!clickedInside && this.menuOpen && window.innerWidth <= 768) {
+      this.menuOpen = false;
+    }
+  }
 }
